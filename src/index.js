58062faf379183890287bb6c8eb8e4460a21e7f8
Frit027 <Frit_027@mail.ru>
2024-10-02 14:30:04 +0300

    created script to handle clicking a button to show/hide text

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const classes = {
+    textContainer: 'text-container',
+    text: 'text-container__text',
+    maxHeightTextContainer: 'text-container_max-height',
+    button: 'footer__more-text-button',
+    arrow: 'footer__arrow',
+    arrowUp: 'footer__arrow_up',
+};
+
+const initScript = () => {
+  const button = document.querySelector(`.${classes.button}`);
+  const arrow = button.querySelector(`.${classes.arrow}`);
+  const textContainer = document.querySelector(`.${classes.textContainer}`);
+  const text = textContainer.querySelector(`.${classes.text}`);
+
+  button.addEventListener('click', () => {
+      arrow.classList.toggle(classes.arrowUp);
+
+      textContainer.style.maxHeight = textContainer.classList.contains(classes.maxHeightTextContainer)
+        ? `${text.offsetHeight}px`
+        : '120px';
+      textContainer.classList.toggle(classes.maxHeightTextContainer);
+  });
+};
+
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', initScript);
+} else {
+    initScript();
+}
