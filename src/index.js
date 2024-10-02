9fd94f97cae59643c3195234c954cc77b43f0112
Frit027 <Frit_027@mail.ru>
2024-10-02 22:41:40 +0300

    added comments

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+/**
+ * Имена CSS-классов, используемых в скрипте
+ */
 const classes = {
     textContainer: 'text-container',
     text: 'text-container__text',
@@ -8,23 +11,50 @@ const classes = {
     arrowUp: 'footer__arrow_up',
 };
 
+/**
+ * Высота текста по умолчанию, чтобы отображались только его первые 5 строк
+ */
+const DEFAULT_MAX_TEXT_HEIGHT = 120;
+
+/**
+ * Основной скрипт страницы
+ */
 const initScript = () => {
-  const button = document.querySelector(`.${classes.button}`);
-  const arrow = button.querySelector(`.${classes.arrow}`);
-  const textContainer = document.querySelector(`.${classes.textContainer}`);
-  const text = textContainer.querySelector(`.${classes.text}`);
+    // Круглая кнопка для показа/скрытия текста
+    const button = document.querySelector(`.${classes.button}`);
 
-  button.addEventListener('click', () => {
-      arrow.classList.toggle(classes.arrowUp);
-      textContainer.classList.toggle(classes.gradient);
+    // Стрелка внутри круглой кнопки
+    const arrow = button.querySelector(`.${classes.arrow}`);
 
-      textContainer.style.maxHeight = textContainer.classList.contains(classes.maxHeightTextContainer)
-        ? `${text.offsetHeight}px`
-        : '120px';
-      textContainer.classList.toggle(classes.maxHeightTextContainer);
-  });
+    // HTML-элемент, внутри которого находится другой HTML-элемент с текстом
+    const textContainer = document.querySelector(`.${classes.textContainer}`);
+
+    // HTML-элемент, содержащий текст
+    const text = textContainer.querySelector(`.${classes.text}`);
+
+    const handleShowHideText = () => {
+        // Меняем класс, чтобы перевернуть стрелку
+        arrow.classList.toggle(classes.arrowUp);
+
+        // Убираем/добавляем градиент при раскрытии/скрытии текста
+        textContainer.classList.toggle(classes.gradient);
+
+        // Задаём максимальную высоту элемента, чтобы открытие/закрытие текста происходило корректно
+        textContainer.style.maxHeight = textContainer.classList.contains(classes.maxHeightTextContainer)
+            ? `${text.offsetHeight}px`
+            : `${DEFAULT_MAX_TEXT_HEIGHT}px`;
+
+        // Убираем/добавляем CSS-класс с макс. значением высоты
+        textContainer.classList.toggle(classes.maxHeightTextContainer);
+    };
+
+    // Обработка клика по кнопке
+    button.addEventListener('click', handleShowHideText);
 };
 
+/**
+ * Точка входа скрипта
+ */
 if (document.readyState === 'loading') {
     document.addEventListener('DOMContentLoaded', initScript);
 } else {
